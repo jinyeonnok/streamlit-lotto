9e72de01b6ec41f4a270e46d61119904879a81c3
Rogio <jjinyeonnok@gmail.com>
2024-11-04 23:06:49 +0900

    'move'

--- my_html/tab1.py
+++ my_html/tab1.py
@@ -82,19 +82,33 @@ def display_current_numbers(lotto_instance,최근회차,전체기록):
         
     # Streamlit에서 제목 설정
     st.title("로또 번호의 연속 미출현 횟수")
-    
-        
+
+    st.markdown(
+        """
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400&display=swap');
+
+            body {
+                font-family: 'Noto Sans KR', sans-serif;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
         
     # x축 및 y축 설정
     x = 통계["번호"]
     y = 통계["연속 미출현 횟수"]
-    
+        
     # 색상 설정
     colors = plt.cm.coolwarm(np.interp(y, (y.min(), y.max()), (0, 1)))
     
     # 그래프 그리기
     plt.figure(figsize=(12, 6))
     bars = plt.bar(x, y, color=colors)
+    
+    # x축의 눈금 설정 (1부터 45까지)
+    plt.xticks(ticks=x, labels=x)
     
     # 그래프 레이블 및 제목 설정
     plt.title("각 번호에 대한 연속 미출현 횟수")
@@ -105,7 +119,6 @@ def display_current_numbers(lotto_instance,최근회차,전체기록):
     for bar in bars:
         yval = bar.get_height()
         plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), ha='center', va='bottom')
-
     
     # 그래프를 Streamlit에 표시
     st.pyplot(plt)
